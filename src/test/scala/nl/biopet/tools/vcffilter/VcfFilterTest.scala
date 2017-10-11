1a8ce522d2b90ae0f61106eb9c2b68bbc849905a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:35 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/vcffilter/VcfFilterTest.scala
+++ src/test/scala/nl/biopet/tools/vcffilter/VcfFilterTest.scala
@@ -7,7 +7,7 @@ object VcfFilterTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      VcfFilter.main(Array())
     }
   }
 }
